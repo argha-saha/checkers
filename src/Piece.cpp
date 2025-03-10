c46e9285595729da34f9d05d456b8e4ccb237c92
argha-saha <saha56@purdue.edu>
2025-03-10 15:06:04 -0400

    Defined constructor and getters for piece class

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -1,6 +1,21 @@
 #include "Piece.hh"
 
-Piece::Piece(int x, int y, sf::Color color) {
+Piece::Piece(int x, int y, sf::Color color)
+     : x(x), y(y), alive(true), 
+       king(false), color(color) {}
 
+void Piece::getX() const {
+    return x;
 }
 
+void Piece::getY() const {
+    return y;
+}
+
+void Piece::isAlive() const {
+    return alive;
+}
+
+void Piece::isKing() const {
+    return king;
+}
